8a986c0b2815490cbcdf9b96ddced7a64b27d714
Chandra Sukmagalih Arifin <chandrachansa@gmail.com>
2023-10-25 20:45:40 +0700

    ..

---
+++ projects/sm-common-lib/src/lib/index.ts
@@ -0,0 +1,5 @@
+export * from './component/custom-grid/custom-grid.component';
+export * from './component/custom-grid2/custom-grid2.component';
+export * from './sm-common-lib.component';
+export * from './sm-common-lib.module';
+export * from './sm-common-lib.service';
--- projects/sm-common-lib/src/public-api.ts
+++ projects/sm-common-lib/src/public-api.ts
@@ -2,8 +2,4 @@
  * Public API Surface of sm-common-lib
  */
 
-export * from './lib/component/custom-grid/custom-grid.component';
-export * from './lib/component/custom-grid2/custom-grid2.component';
-export * from './lib/sm-common-lib.component';
-export * from './lib/sm-common-lib.module';
-export * from './lib/sm-common-lib.service';
+export * from './lib/index';
